c8cf932a89fc7efa0b67b24b2ad84e86ee1ff6ee
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-10-14 19:05:00 -0400

    AST classes/interfaces written

---
+++ application/src/ASTFactory.ts
@@ -0,0 +1,10 @@
+export interface IASTFactory {
+    buildAST(jsonFile : JSON)
+}
+
+export class ASTFactory implements IASTFactory {
+    buildAST(jsonFile: JSON) {
+        throw new Error("Method not implemented.");
+    }
+
+}
---
+++ application/src/SubmissionAST.ts
@@ -0,0 +1,49 @@
+import {IAnalysisResult} from './AnalysisResult';
+
+export interface ISubmissionAST {
+    compare(otherSubmissionAST : ISubmissionAST) : IAnalysisResult
+    add(submissionAST : ISubmissionAST) : void
+    remove(submissionAST : ISubmissionAST) : void
+    getChild(submissionAST : ISubmissionAST) : void
+    getValue() : String
+}
+
+export class SubmissionASTNode implements ISubmissionAST {
+    compare(otherSubmissionAST: ISubmissionAST): IAnalysisResult {
+        throw new Error('Method not implemented.');
+    }
+    add(submissionAST: ISubmissionAST): void {
+        throw new Error('Method not implemented.');
+    }
+    remove(submissionAST: ISubmissionAST): void {
+        throw new Error('Method not implemented.');
+    }
+    getChild(submissionAST: ISubmissionAST): void {
+        throw new Error('Method not implemented.');
+    }
+    getValue(): String {
+        throw new Error('Method not implemented.');
+    }
+
+}
+
+export class SubmissionASTLeaf implements ISubmissionAST {
+    compare(otherSubmissionAST: ISubmissionAST): IAnalysisResult {
+        throw new Error('Method not implemented.');
+    }
+    getValue(): String {
+        throw new Error('Method not implemented.');
+    }
+
+    //Unsuported operations
+    add(submissionAST: ISubmissionAST): void {
+        throw new Error('Method not Supported.');
+    }
+    remove(submissionAST: ISubmissionAST): void {
+        throw new Error('Method not Supported.');
+    }
+    getChild(submissionAST: ISubmissionAST): void {
+        throw new Error('Method not Supported.');
+    }
+
+}
