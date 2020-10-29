b99ceabb11583c1dba2ae4977e64ffd5660c04d1
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-10-29 18:40:46 -0400

    BPB-11 Created tests for AssignmentListComponent

--- bpb-front/src/__tests__/test.spec.js
+++ bpb-front/src/__tests__/test.spec.js
@@ -1,5 +1,52 @@
 import React from 'react';
+import {render, unmountComponentAtNode} from 'react-dom';
+import { act } from "react-dom/test-utils";
+import AssignmentListComponent from "../AssignmentListComponent";
 
 it('something or other callback', () => {
-    expect(true).toBe(false);
+    expect(true).toBe(true);
+});
+
+it('Should display no assignments if none exist', () =>{
+    let container = document.createElement('div');
+    document.body.appendChild(container);
+
+    act(() =>{
+        render(<AssignmentListComponent/>, container);
+    });
+
+    expect(container.getElementsByClassName('assignment-card').length).toBe(0);
+});
+
+it('Should display one assignments if one exists', () => {
+    let container = document.createElement('div');
+    document.body.appendChild(container);
+
+    act(() =>{
+        render(<AssignmentListComponent/>, container);
+    });
+
+    expect(container.getElementsByClassName('assignment-card').length).toBe(1);
+});
+
+it('Should display 3 assignments if 3 exists', () => {
+    let container = document.createElement('div');
+    document.body.appendChild(container);
+
+    act(() =>{
+        render(<AssignmentListComponent/>, container);
+    });
+
+    expect(container.getElementsByClassName('assignment-card').length).toBe(3);
+});
+
+it('Should show a link that directs user to the assignment creation page', () => {
+    let container = document.createElement('div');
+    document.body.appendChild(container);
+
+    act(() =>{
+        render(<AssignmentListComponent/>, container);
+    });
+    //TODO enter link
+    expect(container.getElementsByClassName('new-assignment-btn')).toBe('LINK');
 });
