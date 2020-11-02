2906407c13f07a1ca5b36908f6f566c1ff96aac8
Chris Martin <chris_martin@hphc.org>
2020-11-01 19:01:48 -0500

    BPB-12 fix: Fixed synced folders, moved deployment location to default

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,6 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo npm install -g npm@latest
-cd bpb-front
+cd /vagrant/bpb-front/
 npm install
 npm start
