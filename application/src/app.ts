1b179cd46a6c14b2437adcd4c19bb9e5ac75e747
Chris Martin <chris_martin@hphc.org>
2020-11-02 10:36:35 -0500

    BPB-12 feat: Add backend hello world

--- application/src/app.ts
+++ application/src/app.ts
@@ -4,6 +4,10 @@ let app = express();
 
 app.use(express.json());
 
+app.get('/', function(req, res){
+   res.send("Hello from BPB-back!");
+});
+
 app.listen(8080, () => {
     console.log(`Server is listening on port 8080`);
 });
