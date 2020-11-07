4d35bb3a5ba73ab2f314fb359e193fbcb654106d
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-07 16:56:40 -0500

    BPB-43 REFACTOR: relocate test folder inside of src

--- bpb-front/src/test/index.spec.js
+++ bpb-front/src/test/index.spec.js
@@ -10,7 +10,7 @@ import { expect } from 'chai';
 describe('index.js', () => {
     it('should render AssignmentListComponent', () => {
         const component = renderer.create(
-            <StaticRouter location='/' context={}>
+            <StaticRouter location='/' context={{}}>
                 <AssignmentListComponent/>
             </StaticRouter>
         )
