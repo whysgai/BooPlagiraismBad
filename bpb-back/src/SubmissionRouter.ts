33b6511d0877d258ab97b8436c91b05adda7b3c2
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-07 14:28:57 -0500

    BPB-49 feat: Add example compare endpoint result

--- bpb-back/src/SubmissionRouter.ts
+++ bpb-back/src/SubmissionRouter.ts
@@ -9,11 +9,23 @@ class SubmissionRouter extends AbstractRouter implements IRouter {
   }
 
   setupRoutes() {
-    this.router.get("helloworld",this.getHelloWorldFn);
+    this.router.get("/helloworld",this.getHelloWorldFn);
+    this.router.get("/compare/placeholder",this.getPlaceholderAnalysisResultFn);
   }
   
   getHelloWorldFn = async function(req : Express.Request,res : any){
     res.send({"data":"hello from the bpb-back submission router!!"});
+  }
+
+  //TODO: Replace these
+  //Hardcoded endpoints for front-end development purposes
+  getPlaceholderAnalysisResultFn = async function(rq : Express.Request,res :any){
+    res.send({
+        "matches":[
+            {"fromSubmission":"id1","toSubmission":"id2","fromFile":"test","toFile":"test","fromStart":1,"fromEnd":2,"toStart":3,"toEnd":6,"type":"BasicMatch","description":"Test Description for match 1"},
+            {"fromSubmission":"id1","toSubmission":"id2","fromFile":"test2","toFile":"test3","fromStart":14,"fromEnd":22,"toStart":30,"toEnd":90,"type":"BasicMatch","description":"Test Description for match 2"}
+        ] 
+      });
   }
 }
 
