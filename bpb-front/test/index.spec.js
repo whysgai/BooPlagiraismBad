b2b6ab5a228469338956389374e4ffce0babee24
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-07 16:46:25 -0500

    BPB-43 TEST: implement first router test

---
+++ bpb-front/test/index.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {render, unmountComponentAtNode} from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import AssignmentListComponent from '../AssignmentListComponent';
+import renderer from 'react-test-renderer';
+import { Link } from 'react-router-dom'
+import { StaticRouter } from 'react-router'
+import { expect } from 'chai';
+
+describe('index.js', () => {
+    it('should render AssignmentListComponent', () => {
+        const component = renderer.create(
+            <StaticRouter location='/' context={}>
+                <AssignmentListComponent/>
+            </StaticRouter>
+        )
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+// add additional testing for onclicking nav bar
