b87dd58856dea9b95a328dd29e8ef3550fd41a81
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-06 22:24:54 -0500

    BPB-44 feat: Add AppConfig class

---
+++ bpb-back/src/AppConfig.ts
@@ -0,0 +1,8 @@
+import { Connection } from "mongoose";
+
+export class AppConfig {
+    dbConnection : Connection;
+    public static appName : string = 'bpb-back';
+    public static port : number = 8080;
+    public static dbConnectionString : string = 'mongodb://127.0.0.1:27017/bpb';
+}
