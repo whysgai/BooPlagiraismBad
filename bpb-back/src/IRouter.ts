b181a478e4dece94f8c3ca7e92b615b2e90023ed
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-07 13:57:10 -0500

    BPB-49 feat: Rework IRouter

--- bpb-back/src/IRouter.ts
+++ bpb-back/src/IRouter.ts
@@ -1,11 +1,7 @@
 import express, { Router } from "express";
 
 interface IRouter {
-    setup() : void;
-    postFn(req : Express.Request, res : any) : Promise<void>;
-    getFn(req : Express.Request, res : any) : Promise<void>;
-    putFn(req : Express.Request, res : any) : Promise<void>;
-    deleteFn(req : Express.Request, res : any) : Promise<void>;
+    setupRoutes() : void;
 }
 
 export default IRouter;
