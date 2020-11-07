e27d091f6651e14c51b9240c0045b18c3ffb53d5
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-07 16:42:21 -0500

    BPB-43 FEAT: create placeholders for front end component pages

---
+++ bpb-front/src/AssignmentListComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AssignmentListComponent.css';
+
+function AssignmentListComponent() {
+  return (
+    <div className="App">
+      AssignmentListComponent
+    </div>
+  );
+}
+
+export default AssignmentListComponent;
---
+++ bpb-front/src/HelpComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HelpComponent() {
+  return (
+    <div className="App">
+      HelpComponent
+    </div>
+  );
+}
+
+export default HelpComponent;
