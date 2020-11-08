823c894051a547b65b2f288b3de3ab8f32ee25ca
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-08 00:18:24 -0500

    BPB-52 test: Add Submission tests

---
+++ bpb-back/test/Submission.spec.ts
@@ -0,0 +1,15 @@
+describe("Submission.ts",() => {
+
+    describe("getId()",() => {
+        it("Should return the submission’s id");
+    });
+
+    describe("getName()",() => {
+        it("Should return the submission’s name");
+    });
+
+    describe("compare()",() => {
+        it("Should return a valid AnalysisResult if comparator submission is valid");
+        it("Should throw an appropriate error if comparator submission is invalid");
+    });
+});
