a879ce65f0d1524ab77fa2d9c64b75174710fbfa
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-07 22:38:43 -0500

    BPB-52 test: Make SubmissionManager/DAO testable

--- bpb-back/src/SubmissionDAO.ts
+++ bpb-back/src/SubmissionDAO.ts
@@ -3,6 +3,7 @@ import {Connection} from 'mongoose'
 
 export interface ISubmissionDAO {
     createSubmission() : ISubmission;
+    readSubmissions() : ISubmission[];
     readSubmission(submissionID : String) : ISubmission;
     updateSubmission(submissionID : String, submission : ISubmission) : ISubmission;
     deleteSubmission(submissionID : String) : ISubmission;
@@ -19,13 +20,18 @@ export class SubmissionDAO implements ISubmissionDAO {
     createSubmission(): ISubmission {
         throw new Error('Method not implemented.');
     }
+    
+    readSubmissions(): ISubmission[] {
+        return []; //TODO (also add to interface)
+    }
+
     readSubmission(submissionID: String): ISubmission {
-        throw new Error('Method not implemented.');
+        return undefined; //TODO
     }
     updateSubmission(submissionID: String, submission: ISubmission) : ISubmission {
         throw new Error('Method not implemented.');
     }
     deleteSubmission(submissionID: String): ISubmission {
-        throw new Error('Method not implemented.');
+        return undefined;
     }
 }
