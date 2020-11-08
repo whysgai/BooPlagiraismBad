be5c40fea92c2b51bcbbe1e433317eecc143aa4b
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-08 00:41:25 -0500

    BPB-52 fix: Fix IRouter condensation

--- bpb-back/src/IRouter.ts
+++ bpb-back/src/IRouter.ts
@@ -1,17 +1,8 @@
 import express, { Router } from "express";
 
-export interface IRouter {
+interface IRouter {
     setupRoutes() : void;
 }
 
-export abstract class AbstractRouter {
-    
-    protected router : Router;
-    route : string;
 
-    constructor(app : any, route : string) {
-        this.router = express.Router();
-        this.route = route;
-        app.use(route,this.router);
-    }
-}
+export default IRouter;
