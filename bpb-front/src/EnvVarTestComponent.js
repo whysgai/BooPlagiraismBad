e363241ae1a759e06e9666294e80e9d8200cc864
Will Cohen <will.r.cohen@gmail.com>
2020-11-09 15:10:13 -0500

    BPB-45 docs: Add explanatory comments pertaining to env variables

--- bpb-front/src/EnvVarTestComponent.js
+++ bpb-front/src/EnvVarTestComponent.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 function EnvVarTestComponent() {
     return (
+        // To access env variables, simply use 'process.env.<VARIABLE>'
+        // React can only access variables with 'REACT_APP_' prefix
         <p value="bang!">{process.env.REACT_APP_TESTVAR}</p>
     );
     
