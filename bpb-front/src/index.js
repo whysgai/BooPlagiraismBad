b50d46d7b9a49978c00c8e1db20b6f26f8eb36e6
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-09 18:45:49 -0500

    BPB-50 FEAT: add navbar to react router

--- bpb-front/src/index.js
+++ bpb-front/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
+//import "font-awesome/css/font-awesome.min";
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import AssignmentListComponent from './AssignmentListComponent';
-import ComparisonComponent from './ComparisonComponent'
-import CreateAssignmentComponent from './CreateAssignmentComponent'
-import CreateSubmissionComponent from './CreateSubmissionComponent'
-import HelpComponent from './HelpComponent'
-import SubmissionListComponent from './SubmissionListComponent'
+import ComparisonComponent from './ComparisonComponent';
+import CreateAssignmentComponent from './CreateAssignmentComponent';
+import CreateSubmissionComponent from './CreateSubmissionComponent';
+import HelpComponent from './HelpComponent';
+import SubmissionListComponent from './SubmissionListComponent';
 import * as serviceWorker from './serviceWorker';
+import NavbarComponent from './NavbarComponent';
+//import "../node_modules/bootstrap/dist/css/bootstrap.min";
+//require('bootstrap');
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/" component={AssignmentListComponent}/>
+    <NavbarComponent />
+    <Route exact path="/" component={AssignmentListComponent}/>
     <Route path="/CreateAssignment" component={CreateAssignmentComponent}/>
     <Route path="/Assignment/:AssignmentID" component={SubmissionListComponent}/>
     <Route path="/CreateSubmission" component={CreateSubmissionComponent}/>
