5641273f50bb31b6d3011e027a888d988eec708e
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-10 19:06:06 -0500

    Update AssignmentListComponent.js

--- bpb-front/src/AssignmentListComponent.js
+++ bpb-front/src/AssignmentListComponent.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './AssignmentListComponent.css';
 
-function AssignmentListComponent() {
-  return (
-    <div className="App">
-      AssignmentListComponent
-    </div>
-  );
+export default class AssignmentListComponent extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        AssignmentListComponent
+      </div>
+    );
+  }
 }
-
-export default AssignmentListComponent;
