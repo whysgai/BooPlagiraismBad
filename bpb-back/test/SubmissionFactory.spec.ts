c2316961e9317547adef24953eb0a6d2b87e65ec
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-11 13:13:17 -0500

    BPB-34 test: Add tests for SubmissionFactory, Submission

--- bpb-back/test/SubmissionFactory.spec.ts
+++ bpb-back/test/SubmissionFactory.spec.ts
@@ -1,5 +1,19 @@
+import {SubmissionFactory} from "../src/SubmissionFactory"
+import {Submission} from "../src/Submission"
+import { expect } from "chai";
+
 describe("SubmissionFactory.ts",() => {
+    
     describe("buildSubmission()",() => {
         it("Should return a valid Submission with the expected properties");
+
+            var factory = new SubmissionFactory();
+            var id = "test";
+            var name = "testname";
+            var submission = factory.buildSubmission(id,name);
+            expect(submission).is.not.undefined;
+            expect(submission.getId()).to.equal(id);
+            expect(submission.getName()).to.equal(name);
+        });
     });
 });
