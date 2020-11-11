7f84c94cf69f26c12edd734d3dd11f1369a5d703
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-11 12:49:07 -0500

    BPB-34 test: Add tests for AssignmentFactory

--- bpb-back/test/AssignmentFactory.spec.ts
+++ bpb-back/test/AssignmentFactory.spec.ts
@@ -1,5 +1,17 @@
+import { expect } from "chai";
+import { AssignmentFactory } from "../src/AssignmentFactory";
+
 describe("AssignmentFactory.ts",() => {
+
     describe("buildAssignment()",() => {
-        it("Should return a valid Assignment with the expected properties");
+        it("Should return a valid Assignment with the expected properties",() => {
+
+            var factory = new AssignmentFactory();
+            var assignment = factory.buildAssignment("a","b");
+
+            expect(assignment.getID()).to.equal("a");
+            expect(assignment.getName()).to.equal("b");
+            expect(assignment.getSubmissionIDs()).to.be.empty;
+        });
     });
 });
