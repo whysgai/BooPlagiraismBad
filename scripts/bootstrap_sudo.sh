ac0b53ce9e4ffb88ba36df3453fa92455e02a9a1
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-11 16:29:14 -0500

    BPB-58 infra: shift react env vars to pm2

--- scripts/bootstrap_sudo.sh
+++ scripts/bootstrap_sudo.sh
@@ -9,16 +9,11 @@ sudo apt-get install -y mongodb
 sudo npm install -g npm@latest
 sudo npm install -g pm2@latest
 sudo npm install -g typescript@latest # Required to run tsc to compile back-end
-sudo rm -rf /vagrant/bpb-back/dist # Remove any previously compiled back-end files in dist
+rm -rf /vagrant/bpb-back/dist/* # Remove any previously compiled back-end files in dist
+sudo chown vagrant:vagrant /home/vagrant/.pm2/rpc.sock /home/vagrant/.pm2/pub.sock
 
 #Set up database service
 sudo mv /vagrant/scripts/mongodb.service /lib/systemd/mongodb.service
 sudo systemctl enable mongodb
-
-# Set env variable(s)
-# To be accessed by react, env variables must include REACT_APP_ prefix
-sudo echo "REACT_APP_TESTVAR='Hello, World!'" >> /etc/environment
-sudo echo "REACT_APP_CLTADDR='client placeholder address'" >> /etc/environment
-sudo echo "REACT_APP_SRVADDR='server placeholder address'" >> /etc/environment
 
 exit 0
