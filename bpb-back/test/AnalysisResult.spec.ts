f37cfe05dfe23623c707c9fbffbec60149f64005
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-13 18:01:29 -0500

    BPB-63 docs: Add todos

--- bpb-back/test/AnalysisResult.spec.ts
+++ bpb-back/test/AnalysisResult.spec.ts
@@ -5,6 +5,7 @@ import { AnalysisResultEntry } from "../src/AnalysisResultEntry";
 describe.skip("AnalysisResult.ts",() => {
     describe("asJSON / addMatch",() => {
         it("Should return a valid JSON object with the expected properties",() => {
+            //TODO: Expected property changes may make this test fail!
             var expected = '[[{"sub_id":"subid1","file_path":"/test/file.java","context":"method","start":1,"end":2,"hash":"245rr1","text":"void test() { }"},{"sub_id":"subid2","file_path":"/test/file2.java","context":"method","start":5,"end":6,"hash":"423qq1","text":"void similar() { }"}]]'
             var expectedJSON = JSON.parse(expected);
             var analysisResult = new AnalysisResult();
