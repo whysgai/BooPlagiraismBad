b89d56336ecfbb544fbf5789ec74eb65ac619c2a
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-13 13:48:50 -0500

    BPB-50 TEST: finish tests for NavbarComponent

--- bpb-front/src/NavbarComponent.js
+++ bpb-front/src/NavbarComponent.js
@@ -5,18 +5,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default class NavbarComponent extends React.Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <Link class="navbar-brand" to="/">BooCheatingBad</Link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to="/">BooCheatingBad</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <Link class="nav-link" to="/">Assignments<span class="sr-only">(current)</span></Link>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/" id="AssignmentsLink">Assignments<span className="sr-only">(current)</span></Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/Help">Help</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/Help" id="HelpLink">Help</Link>
                         </li>
                     </ul>
                 </div>
