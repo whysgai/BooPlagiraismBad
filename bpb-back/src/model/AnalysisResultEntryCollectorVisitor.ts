9ed5fca347f7dc108051dae264d0131e9a6ae0bd
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-13 18:05:42 -0500

    BPB-63 infra: rename visitor

--- bpb-back/src/model/AnalysisResultEntryCollectorVisitor.ts
+++ bpb-back/src/model/AnalysisResultEntryCollectorVisitor.ts
@@ -1,11 +1,11 @@
 import {AnalysisResultEntry} from "../AnalysisResultEntry"
 
-interface IAnalysisResultCollectorVisitor {
+interface IAnalysisResultEntryCollectorVisitor {
     getAnalysisResultEntries(): AnalysisResultEntry[]    
 }
 
 //TODO: Add extension of AbstractParseTreeVisitor (including actual visit methods, etc)
-class AnalysisResultCollectorVisitor implements IAnalysisResultCollectorVisitor{
+class AnalysisResultEntryCollectorVisitor implements IAnalysisResultEntryCollectorVisitor{
     
     constructor(filePath : String) {
         //TODO: filePath is the path to the file being processed. Don't need to retrieve contents again (done externally) - this is for inclusion in AnalysisResultEntry metadata
