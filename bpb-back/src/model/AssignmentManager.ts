afb347a964ce555ac1a0daba3cf559b59dee366d
Will Cohen <will.r.cohen@gmail.com>
2020-11-14 11:28:03 -0500

    BPB-29 refactor: Implement async methods and promise returns

--- bpb-back/src/model/AssignmentManager.ts
+++ bpb-back/src/model/AssignmentManager.ts
@@ -1,26 +1,34 @@
 import { IAssignment } from "./Assignment";
+import { IAssignmentDAO } from "./AssignmentDAO";
 
 /**
  * Represents a controller for Assignment objects.
  */
 export interface IAssignmentManager {
-    getAssignments() : IAssignment[]
-    createAssignment(data : JSON) : IAssignment
-    updateAssignment(assignment : IAssignment, data : JSON) : void
-    deleteAssignment(assignment : IAssignment) : void
+    getAssignments() : Promise<IAssignment[]>;
+    createAssignment(data : JSON) : Promise<IAssignment>;
+    updateAssignment(assignment : IAssignment, data : JSON) : Promise<void>;
+    deleteAssignment(assignment : IAssignment) : Promise<void>;
 }
 
 export class AssignmentManager implements IAssignmentManager {
-    getAssignments(): IAssignment[] {
+    
+    private assignmentDAO : IAssignmentDAO;
+
+    constructor(assignmentDAO: IAssignmentDAO) {
+        this.assignmentDAO = assignmentDAO;
+    }    
+    
+    async getAssignments(): Promise<IAssignment[]> {
         throw new Error("Method not implemented.");
     }
-    createAssignment(data : JSON): IAssignment {
+    async createAssignment(data : JSON): Promise<IAssignment> {
         throw new Error("Method not implemented.");
     }
-    updateAssignment(assignment: IAssignment, data : JSON) : void {
+    async updateAssignment(assignment: IAssignment, data : JSON) : Promise<void> {
         throw new Error("Method not implemented.");
     }
-    deleteAssignment(assignment : IAssignment) : void {
+    async deleteAssignment(assignment : IAssignment) : Promise<void> {
         throw new Error("Method not implemented.");
     }
 }
