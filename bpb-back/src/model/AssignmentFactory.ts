bf41189dda3bdfec6a204cc0ca72e641ee170779
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-14 16:30:19 -0500

    BPB-29 fix: remove unused function

--- bpb-back/src/model/AssignmentFactory.ts
+++ bpb-back/src/model/AssignmentFactory.ts
@@ -1,6 +1,4 @@
 import { Assignment, IAssignment } from "./Assignment";
-import { AssignmentDAO } from "./AssignmentDAO";
-import { AssignmentManager, IAssignmentManager } from "./AssignmentManager";
 
 export interface IAssignmentFactory {
     //buildAssignment(id : String, name: String) : IAssignment
@@ -11,10 +9,5 @@ export interface IAssignmentFactory {
 export class AssignmentFactory implements IAssignmentFactory {
      static buildAssignment(id : String, name: String): IAssignment {
          return new Assignment(id,name);
-    }
-
-    // TODO REMOVE THIS
-    static async buildAssignmentManager() : Promise<IAssignmentManager> {
-        return Promise.resolve(new AssignmentManager(new AssignmentDAO()));
     }
 }
