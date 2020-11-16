1c2c600481422b399983996fb5b02fc9f7abca3f
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-11-16 16:00:52 -0500

    BPB-55 docs: added comments to all methods, classes, and interfaces

--- bpb-back/src/model/AnalysisResultEntryCollectorVisitor.ts
+++ bpb-back/src/model/AnalysisResultEntryCollectorVisitor.ts
@@ -1,3 +1,8 @@
+//AnalysisResultEntryCollectorVisitor.ts
+/**
+ * Contains {@link IAnalysisResultEntryCollectorVisitor } and {@link @AnalysisResultEntryCollectorVisitor}
+ * @packageDocumentation
+ */
 import {AnalysisResultEntry} from "../AnalysisResultEntry";
 import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor';
 import { ParseTree } from 'antlr4ts/tree/ParseTree';
@@ -7,18 +12,45 @@ import { ParseTreeVisitor, RuleNode } from "antlr4ts/tree";
 import { ISubmission } from "./Submission";
 import { JavaParser } from 'java-ast/dist/parser/JavaParser'
 
+/**
+ * Interface for an AnalysisResultEntryCollectorVisitor. Extends the {@link ParseTreeVisitor interface}
+ */
 export interface IAnalysisResultEntryCollectorVisitor extends ParseTreeVisitor<any> {
+    /**
+     * returns the AnalysisResultEntry[] gathered by the visitor.
+     */
     getAnalysisResultEntries(): AnalysisResultEntry[];
+    
+    /**
+     * Returns the filePath of the file whose AST the visitor is visiting.
+     */
     getFilePath() : string;
+    
+    /**
+     * returns the ISubmission implementing object that this visitor is working on the behalf of.
+     */
     getSubmission() : ISubmission;
+    
+    /**
+     * returns True if the {@link IAnalysisResultEntryCollectorVisitor} has performed a visit(). 
+     * Returns False otherwise.
+     */
     hasVisited() : boolean;
 }
 
 //TODO: change <any> to appropriate type
+/**
+ * Implementation of the {@link IAnalysisResultEntryCollectorVisitor} interface. Extends {@link AbstractParseTreeVisitor} 
+ * to visit a given {@link ParseTree}, and creates an array of {@link AnalysisResultEntry | analysisResultEntries} of 
+ * relevant nodes while visiting.
+ */
 export class AnalysisResultEntryCollectorVisitor extends AbstractParseTreeVisitor<any> implements IAnalysisResultEntryCollectorVisitor  {
     private analysisResultEntries : AnalysisResultEntry[];
     private visited : boolean;
 
+    /**
+     * See {@inheritdoc}
+     */
     protected defaultResult() {
         return this.analysisResultEntries;
     }
@@ -32,18 +64,34 @@ export class AnalysisResultEntryCollectorVisitor extends AbstractParseTreeVisito
         this.analysisResultEntries = new Array<AnalysisResultEntry>();
     }
 
+    /**
+     * returns this.submission
+     */
     getSubmission(): ISubmission {
         return this.submission;
     }
 
+    /**
+     * returns this.visited
+     */
     hasVisited(): boolean {
         return this.visited;
     }
 
+    /**
+     * returns this.filePath
+     */
     getFilePath(): string {
         return this.filePath;
     }
 
+    /**
+     * Given a string 'textcontent', creates a hash value using Locality Sensitive Hashing.
+     * Utilizes the {@link https://github.com/trendmicro/tlsh/blob/master/js_ext/tlsh.js | trendmicro/tlsh } 
+     * library for LSH. See changes/updates made to the original library 
+     * {@link https://github.com/turecarlson/tlsh/commits/master/js_ext/tlsh.js | here}
+     * @param textContent 
+     */
     protected getLSHValue(textContent : string) : string {
         let tlsh = new Tlsh();
         tlsh.update(textContent, textContent.length+1);
@@ -51,6 +99,10 @@ export class AnalysisResultEntryCollectorVisitor extends AbstractParseTreeVisito
         return tlsh.hash().toString();
     }
     
+    /**
+     * Creates our AnalysisResultEntry of the given node (ParseTree)
+     * @param parseTree the node from which we will compose an AnalysisResultEntry
+     */
     protected createAnalysisResultEntry(parseTree : ParseTree) : AnalysisResultEntry {
         
         let hashValue = this.getLSHValue(parseTree.toStringTree());
@@ -70,6 +122,13 @@ export class AnalysisResultEntryCollectorVisitor extends AbstractParseTreeVisito
         return new AnalysisResultEntry(submissionId, this.filePath, contextType, lineStart, lineStop, hashValue, textContent);
     }
 
+    /**
+     * Modification of inherited method from {@abstract}
+     * See {@inheritdoc}
+     * Tries to create an AnalysisResultEntry object from the given ParseTree.
+     * If tlsh.hash() throws an error, the AnalysisResultEntry object is not created, and an error is thrown.
+     * @param parseTree The ParseTree to be visited (root node)
+     */
     visit(parseTree : ParseTree) {
         try {
             this.analysisResultEntries.push(this.createAnalysisResultEntry(parseTree)); //Collect our AnalysisResultEntry for the root node
@@ -92,6 +151,14 @@ export class AnalysisResultEntryCollectorVisitor extends AbstractParseTreeVisito
         return returnVal;
     }
 
+    /**
+     * Modification of inherited method from {@abstract}
+     * See {@inheritdoc}
+     * Attempts to create an AnalysisResultEntry object of each child. 
+     * If tlsh.hash() throws an error during {@link getLSHValue}, object creation 
+     * is skipped, and subtree (child) is not visited, for efficiency.
+     * @param node the current node, whose children may be visited.
+     */
     visitChildren(node : RuleNode) {
         let result = this.defaultResult();
         let n = node.childCount;
@@ -123,6 +190,9 @@ export class AnalysisResultEntryCollectorVisitor extends AbstractParseTreeVisito
         return result;
     }
 
+    /**
+     * Returns this.analysisResultEntries. Throws an error if no visit has been executed.
+     */
     getAnalysisResultEntries() : AnalysisResultEntry[] {
         if (this.hasVisited()) {
             return this.analysisResultEntries;
