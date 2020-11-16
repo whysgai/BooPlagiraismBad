618fe680e4567bc805fe64cf20ff2c04c586eb22
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-11-16 13:04:42 -0500

    BPB-72 removed ASTFactory.ts

--- bpb-back/src/model/ASTFactory.ts
+++
@@ -1,9 +0,0 @@
-export interface IASTFactory {
-    //buildAST(jsonFile : JSON)
-}
-
-export class ASTFactory implements IASTFactory {
-    buildAST(jsonFile: JSON) {
-        throw new Error("Method not implemented.");
-    }
-}
