40d9f851b81f727ff2e49acf9a262f5ebbd7c00c
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-16 20:54:09 -0500

    BPB-20 feat: implement SubmissionListComponent

--- bpb-front/src/SubmissionListComponent.tsx
+++ bpb-front/src/SubmissionListComponent.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import SubmissionListItemComponent from './SubmissionListItemComponent'
 
-function SubmissionListComponent({submissions}) {
+function SubmissionListComponent({submissions}: {submissions: Submission[]}) {
   return (
     <div>
       <ul>
         {submissions.map((submission, index) => 
-          <li key={index}> {submission} </li>
+          <li key={index}><SubmissionListItemComponent submission={submission} createSubmission={null}/></li>
         )}
       </ul>
     </div>
