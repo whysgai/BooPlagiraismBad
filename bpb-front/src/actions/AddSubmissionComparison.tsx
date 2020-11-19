70c4e380ed9fbde4be84d8f8e50c7cf1a6172e8c
Will Cohen <will.r.cohen@gmail.com>
2020-11-18 20:45:28 -0500

    BPB-20 refactor: Put like actions with like actions

--- bpb-front/src/actions/AddSubmissionComparison.tsx
+++
@@ -1,8 +0,0 @@
-import Submission from '../types/Submission'
-
-export default function addSubmissionComparison(addSubmission : Submission) {
-    return {
-        type: 'ADD_COMPARE',
-        addSubmission: addSubmission,
-    }
-}
--- bpb-front/src/actions/ComparisonAction.tsx
+++ bpb-front/src/actions/ComparisonAction.tsx
@@ -1,8 +1,15 @@
 import Submission from '../types/Submission'
 
-export default function compareSubmissions(compareSubmissions : Submission[]) {
+export function compareSubmissions(compareSubmissions : Submission[]) {
     return {
         type: 'COMPARE',
         compareSubmissions: compareSubmissions,
     }
 };
+
+export function addSubmissionComparison(addSubmission : Submission) {
+    return {
+        type: 'ADD_COMPARE',
+        addSubmission: addSubmission,
+    }
+}
