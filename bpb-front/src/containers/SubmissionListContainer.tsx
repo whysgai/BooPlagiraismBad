72dbae68ac4e23161244f1691b7c072ba3fa82b5
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-20 19:32:08 -0500

    BPB-20 feat: revert stateToPropertyMapper

--- bpb-front/src/containers/SubmissionListContainer.tsx
+++ bpb-front/src/containers/SubmissionListContainer.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import SubmissionListComponent from '../SubmissionListComponent'
+import { addSubmissionComparison } from '../actions/ComparisonAction';
+import SubmissionReducer from '../reducers/SubmissionReducer'
+import ComparisonReducer from '../reducers/ComparisonReducer'
 
 export const stateToPropertyMapper=(state) => {
     return {
-        tempAssignment: state.SubmissionReducer.tempAssignment,
-        submissionComparison: state.ComparisonReducer.state
+        tempAssignment: SubmissionReducer,
+        submissionComparison: ComparisonReducer
     }
 }
 
 export const dispatchToPropertyMapper=(dispatch) => {
     return {
-        dispatch: dispatch
+        addSubmissionComparison: (submission) => dispatch(addSubmissionComparison(submission)),
     }
 }
 
