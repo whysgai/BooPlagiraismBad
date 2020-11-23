c1dc1bd27c46c8c20c08c50bf34854c08b90f6e1
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-23 17:43:02 -0500

    BPB-33 feat: added assignment builder

--- bpb-back/src/model/Assignment.ts
+++ bpb-back/src/model/Assignment.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema } from "mongoose";
+import { Submission } from "./Submission";
 
 /**
  * Represents an Assignment database model object
@@ -23,6 +24,60 @@ export interface IAssignment {
 }
     
 export class Assignment implements IAssignment {
+
+    /**
+     * Builder for Assignments
+     */
+    static builder = class AssignmentBuilder {
+
+        private name : string;
+        private submissionIds: string[];
+
+        constructor() {
+            this.name = "Name Not Defined";
+            this.submissionIds = [];
+        }
+
+        setName(name : string) : void {
+            this.name = name;
+        }
+
+        setSubmissionIds(submissionIds : string[]) : void {
+            this.submissionIds = submissionIds;
+        }
+
+        build() : IAssignment {
+            var assignment = new Assignment();
+
+            var assignmentModel = Assignment.getStaticModel();
+            var modelInstance = new assignmentModel({"name" : this.name, "submissionIds" : this.setSubmissionIds});
+
+            assignment.setId(modelInstance.id);
+            assignment.setName(modelInstance.name);
+            assignment.setModelInstance(modelInstance);
+            assignment.setSubmissionIds(modelInstance.submissionIds);
+
+            return assignment;
+        }
+
+        //NOTE: Using buildFromExisting overrides all other builder methods
+        buildFromExisting(model : IAssignmentModel) : IAssignment {
+            var assignment = new Assignment();
+
+            if (!model._id || !model.name || !model.submissionIds) {
+                throw new Error("At least one required assignment model property is not present on the provided model");
+            }
+
+            assignment.setId(model._id);
+            assignment.setModelInstance(model);
+            assignment.setName(model.name);
+            assignment.setSubmissionIds(model.submissionIds);
+
+            return assignment;
+        }
+    }
+
+
     
     private static assignmentSchema = new Schema({
         _id:  String,
@@ -33,14 +88,27 @@ export class Assignment implements IAssignment {
     private static assignmentModel = mongoose.model<IAssignmentModel>('Assignment',Assignment.assignmentSchema);
     
     private submissionIds : string[];
+    private modelInstance : IAssignmentModel;
+    private id : string;
+    private name : string;
     
-    constructor(private id : string, private name :string) {
-        this.id = id;
-        this.name = name
-        this.submissionIds = [];
+    protected constructor() {
     }
+
     static getStaticModel() : mongoose.Model<IAssignmentModel> {
         return this.assignmentModel;
+    }
+    protected setId(id : string): void {
+        this.id = id;
+    }
+    protected setModelInstance(modelInstance : IAssignmentModel) : void {
+        this.modelInstance = modelInstance;
+    }
+    protected setName(name : string) : void {
+        this.name = name;
+    }
+    protected setSubmissionIds(submissionIds : string[]) {
+        this.submissionIds = submissionIds;
     }
     getID(): string {
         return this.id;
