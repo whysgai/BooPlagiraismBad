17b09708d8ea9d7de7c0b64f70f8fa122e7efc03
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-24 18:00:11 -0500

    BPB-15 feat: initial implementation of create assignment component

--- bpb-front/src/CreateAssignmentComponent.tsx
+++ bpb-front/src/CreateAssignmentComponent.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { store } from './store';
 
-function CreateAssignmentComponent() {
-  return (
-    <div className="App">
-      CreateAssignmentComponent
-    </div>
-  );
+interface PropsType {
+  name: string
+}
+
+class CreateAssignmentComponent extends React.Component <PropsType, {}> {
+    constructor(props : PropsType) {
+        super(props);
+        this.state = {
+          name: this.props.name
+        };
+        this.onInputchange = this.onInputchange.bind(this);
+  }
+
+  onInputchange(event : ChangeEvent<HTMLInputElement>) {
+    this.setState({
+        [event.target.name]: event.target.value,
+    });
+  }
+
+  callDispatch() {
+    
+  }
+
+  render() {
+    return (
+        <div style={{ textAlign: 'center', paddingTop: '20px' }}>
+            <div style={{textAlign: 'right', paddingRight: '50px', fontWeight: 'bolder'}}>
+                <Link to="/">
+                  x 
+                </Link>
+            </div>
+            <h3>Create Assignment</h3>
+            <br/>
+            <span text-align="center">
+                <h5>Assignment Name:</h5>
+                <input
+                    name="name"
+                    type="text"
+                    value={this.props.name}
+                    onChange={this.onInputchange}
+                />
+                <br />
+                <Link
+                    to="/"
+                    onClick={() => this.callDispatch()}>
+                    Create Assignment
+                </Link>
+            </span>
+        </div>
+    );
+}
 }
 
 export default CreateAssignmentComponent;
