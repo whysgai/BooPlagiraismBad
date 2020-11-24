a1d5c5256a8623eaebc845091748dd3a7e7cb611
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-24 18:02:53 -0500

    BPB-20 feat: add assignment reducer to store

--- bpb-front/src/store.tsx
+++ bpb-front/src/store.tsx
@@ -1,14 +1,15 @@
 import { createStore, combineReducers, Store } from 'redux';
-import { Provider } from 'react-redux';
 import ComparisonReducer from './reducers/ComparisonReducer';
 import SubmissionReducer from './reducers/SubmissionReducer';
+import AssignmentReducer from './reducers/AssignmentReducer';
 
 //import "../node_modules/bootstrap/dist/css/bootstrap.min";
 //require('bootstrap');
 
 const reducers = combineReducers({
   ComparisonReducer: ComparisonReducer,
-  SubmissionReducer: SubmissionReducer
+  SubmissionReducer: SubmissionReducer,
+  AssignmentReducer: AssignmentReducer
 })
 
 export const store: Store = createStore(reducers);
