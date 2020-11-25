ef69cbb4e2df2bd92a9fccbef0d79625d9008182
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-24 21:35:25 -0500

    BPB-14 feat: update styling in assignment list card and add number of submissions for each assignment

--- bpb-front/src/AssignmentListCard.tsx
+++ bpb-front/src/AssignmentListCard.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Assignment from './types/Assignment'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function AssignmentListCard({assignment, createAssignment}:{assignment : Assignment, createAssignment: (arg: String) => void }) {
   return (
     <div className="assignment-list-card">
-      {assignment.name}
+      <span>
+        {assignment.name}
+      </span>
+      <span className='pl-2'>
+        Number of Submissions: {assignment.submissionIds.length}
+      </span>
     </div>
   );
 }
