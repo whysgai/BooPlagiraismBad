eebf337d5018fae248efd8510b2a2ff736f85835
Chris Martin <chris_martin@hphc.org>
2020-11-24 20:08:10 -0500

    BPB-23 test: add ComparisonComponent tests

---
+++ bpb-front/src/test/ComparisonComponent.spec.js
@@ -0,0 +1,27 @@
+describe("ComparisonComponent",() => {
+
+    it("Should render a static title");
+
+    it("Should render a CloseComparisonComponentButton");
+
+    it("Should render ChangeComparisonViewButton");
+
+    it("Should render names of submissions");
+
+    it("Should render Deckard similarity information");
+
+    it("Should render two DocumentVisualizationComponents");
+
+    it("Should render two DirectoryListComponents");
+});
+
+describe("CloseComparisonViewButton",() => {
+    it("Should close the ComparisonComponent and redirect to the ???")
+});
+
+describe("ChangeComparisonViewButton",() => {
+    it("Should switch to Document view if in Snippet view when clicked");
+
+    it("Should switch to Snippet view if in Document view when clicked");
+});
+
