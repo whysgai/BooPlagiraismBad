8023557d04a8bbef578a486a8a101c1fc8f0515f
Chris Martin <chris_martin@hphc.org>
2020-11-24 20:23:08 -0500

    BPB-23 test: add tests

--- bpb-front/src/test/ComparisonComponent.spec.js
+++ bpb-front/src/test/ComparisonComponent.spec.js
@@ -1,23 +1,50 @@
+import { expect } from 'chai';
+import { StaticRouter} from 'react-router';
+
 describe("ComparisonComponent",() => {
+
+    let container;
 
     beforeEach(() => {
         container = document.createElement('div');
         document.body.appendChild(container);
     });
 
-    it("Should render a static title");
+    it("Should render a CloseComparisonComponentButton",() => {
+        act(() =>{
+            render(<StaticRouter><ComparisonComponent/></StaticRouter>, container);
+        });
 
-    it("Should render a CloseComparisonComponentButton");
+        expect(container.getElementsByClassName("close-comparison-btn").length).to.equal(1);
+    });
 
-    it("Should render ChangeComparisonViewButton");
+    it("Should render ChangeComparisonViewButton",() => {
+        act(() =>{
+            render(<StaticRouter><ComparisonComponent/></StaticRouter>, container);
+        });
 
-    it("Should render names of submissions");
+        expect(container.getElementsByClassName("change-comparison-btn").length).to.equal(1);
+    }); 
 
-    it("Should render Deckard similarity information");
+    it("Should render comparison metadata (submission names and Deckard similarity information)",() => {
 
-    it("Should render two DocumentVisualizationComponents");
+    });
 
-    it("Should render two DirectoryListComponents");
+    it("Should render two DocumentVisualizationComponents",() => { 
+        act(() =>{
+            render(<StaticRouter><ComparisonComponent/></StaticRouter>, container);
+        });
+
+        expect(container.getElementsByClassName("document-pane").length).to.equal(2);
+    }); 
+
+    it("Should render two DirectoryListComponents",() => {
+        act(() =>{
+            render(<StaticRouter><ComparisonComponent/></StaticRouter>, container);
+        });
+
+        expect(container.getElementsByClassName("directory-list").length).to.equal(2);
+    });
 });
 
 describe("CloseComparisonViewButton",() => {
