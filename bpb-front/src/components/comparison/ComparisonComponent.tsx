10bf5fb483801e9531d58949f61805e6d29a9a2b
Chris Martin <chris_martin@hphc.org>
2020-11-24 20:38:49 -0500

    BPB-23 feat: Typescript-ify ComparisonComponent

--- bpb-front/src/components/comparison/ComparisonComponent.tsx
+++ bpb-front/src/components/comparison/ComparisonComponent.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import Submission from './types/Submission';
 
-function ComparisonComponent() {
-  return (
-    <div className="App">
-      ComparisonComponent
-    </div>
-  );
+interface PropsType {
+  submissions : Submission[]
+}
+
+/**
+ * Represents a comparison view between two submissions
+ */
+class ComparisonComponent extends React.Component <PropsType, {}> {
+
+  constructor(props : PropsType) {
+    super(props);
+    
+    this.state = {
+      submissions:this.props.submissions
+    };
+  }
+
+  render() {
+    return (
+      <div className="submission-compare-pane">
+        ComparisonComponent
+      </div>
+    );
+  }
 }
 
 export default ComparisonComponent;
