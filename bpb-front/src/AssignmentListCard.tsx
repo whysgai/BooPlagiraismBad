7e9b8a8b72f6925ecf7608058f883060a1562ef3
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-25 10:59:29 -0500

    BPB-21 feat: set current assignment when an assignment has been selected

--- bpb-front/src/AssignmentListCard.tsx
+++ bpb-front/src/AssignmentListCard.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import Assignment from './types/Assignment'
+import { Link } from 'react-router-dom';
+import { store } from './store';
+import {setCurrentAssignment} from './actions/AssignmentAction'
 
-function AssignmentListCard({assignment, createAssignment}:{assignment : Assignment, createAssignment: (arg: String) => void }) {
-  return (
-    <div className="assignment-list-card card" >
-      <div className='card-body'>
-        <span className='card-title'>
-          {assignment.name}
-        </span>
-        <span className='pl-2'>
-          Number of Submissions: {assignment.submissionIds.length}
-        </span>
+interface PropsType {
+  assignment: Assignment
+  createAssignment: (arg: String) => void 
+}
+
+class AssignmentListCard extends React.Component <PropsType,{}> {
+
+//({assignment, createAssignment}:{assignment : Assignment, createAssignment: (arg: String) => void }) 
+
+  constructor(props : PropsType) {
+    super(props)
+    this.state = {
+      assignment: this.props.assignment
+    }
+  }
+  
+  setAssignment() {
+    store.dispatch(setCurrentAssignment('SET_CURRENT_ASSIGNMENT', this.props.assignment))
+  }
+  
+  render() {
+    return (
+      <div className="assignment-list-card card">
+        <Link to='/' className='card-body' onClick={() => this.setAssignment()}>
+          <span className='card-title'>
+            {this.props.assignment.name}
+          </span>
+          <span className='pl-2'>
+            Number of Submissions: {this.props.assignment.submissionIds.length}
+          </span>
+        </Link>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default AssignmentListCard;
