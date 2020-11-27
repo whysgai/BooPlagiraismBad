855581baeedb25d7149932098e5259310912ce4c
Chris Martin <chris_martin@hphc.org>
2020-11-27 17:38:21 -0500

    BPB-99 docs: add main app docs

--- bpb-back/src/AppConfig.ts
+++ bpb-back/src/AppConfig.ts
@@ -3,18 +3,30 @@
  */
 export class AppConfig {
    
+    /**
+     * Returns the configured application port
+     */
     public static port() : string {
         return process.env.APIPORT;
     }
     
+    /**
+     * Returns the configured application file upload directory for submission files
+     */
     public static submissionFileUploadDirectory() : string {
         return process.env.UPLOADDIRECTORY;
     }
 
+    /**
+     * Returns the configured database connection string for the app database
+     */
     public static dbConnectionString() : string {
         return process.env.DBCONNECTIONSTRING;
     }
 
+    /**
+     * Returns the configured maximum size (in bytes) of a single submission file upload
+     */
     public static maxFileUploadSize() : number {
         return Number(process.env.MAXFILEUPLOADSIZE);
     }
--- bpb-back/src/_main.ts
+++ bpb-back/src/_main.ts
@@ -1,5 +1,6 @@
 import App from './App';
 import {AppConfig} from './AppConfig';
 
+//Instantiate and run app
 const app = new App(AppConfig.dbConnectionString(),AppConfig.port());
 app.run();
