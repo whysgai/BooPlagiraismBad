3facfc4c0fe15e17e369c654a646f7a1d6a50961
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-28 15:48:39 -0500

    BPB-21 feat: update postSubmission and add postFile

--- bpb-front/src/services/SubmissionService.tsx
+++ bpb-front/src/services/SubmissionService.tsx
@@ -1,21 +1,24 @@
 import Assignment from "../types/Assignment"
+import Submission from "../types/Submission"
 
-const assignmentURL = 'http://192.168.33.10:8080/'
+const vagrantURL = 'http://192.168.33.10:8080/'
+const formData = new FormData()
 
 export function postSubmission(name : string, assignment: Assignment, files: JSON[]) : void {
-    fetch(`${assignmentURL}submissions`, {
+    fetch(`${vagrantURL}submissions`, {
         method: 'POST',
         body: JSON.stringify({"name": name, "assignment_id": assignment._id}),
         headers: {'content-type': 'application/json'}
-    })
+    }).then(response => response.json()).then(newSubmission => postFile(newSubmission, files))
 }
 
-export function postFile(name : string, assignment: Assignment, files: JSON[]) : void {
-    fetch(`${assignmentURL}submissions/${submission.submissionId}files`, {
-        method: 'POST',
-        body: JSON.stringify({"name": name, "assignment_id": assignment._id}),
-        headers: {'content-type': 'application/json'}
-    })
+export function postFile(submission: Submission, files: JSON[]) : void {
+    for (let file in files) {
+        fetch(`${vagrantURL}submissions/${submission._id}files`, {
+            method: 'POST',
+            body: formData,
+        })
+    }
 }
 
-export default {postSubmission}
+export default {postSubmission, postFile}
