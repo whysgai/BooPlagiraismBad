54eb3e0f2bbd9c1761d2657bdbdf8acaf83eb1ad
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-11-27 19:49:22 -0500

    BPB-94 feat: changed all 'filepath' to 'filename'

--- bpb-back/src/model/AnalysisResult.ts
+++ bpb-back/src/model/AnalysisResult.ts
@@ -13,14 +13,14 @@ export class AnalysisResult implements IAnalysisResult {
     private fileNames : Map<string, string>; // key: submissionId, val: fileName
 
     constructor(private matches : Array<Array<IAnalysisResultEntry>>, private similarityScore : number, 
-        submissionIdA : string, submissionIdB : string, filePathA : string, filePathB : string) {
+        submissionIdA : string, submissionIdB : string, filenameA : string, filenameB : string) {
         
         if(submissionIdA == undefined || submissionIdB == undefined || submissionIdA === "" || submissionIdB === "") {
             throw new Error('Provided submissionId values must not be undefined or empty strings');
         }
         
-        if(filePathA == undefined || filePathB == undefined || filePathA === "" || filePathB === "") {
-            throw new Error('Provided filePath values must not be undefined or empty strings');
+        if(filenameA == undefined || filenameB == undefined || filenameA === "" || filenameB === "") {
+            throw new Error('Provided filename values must not be undefined or empty strings');
         }
 
         if(similarityScore < 0) {
@@ -42,7 +42,7 @@ export class AnalysisResult implements IAnalysisResult {
                 throw new Error("Bad Constructor: the two entries in a match at 'match[*]' must not be the same AnalysisResultEntry object instance.");
             }
             if(match[0].getFileName() === match[1].getFileName()) {
-                throw new Error("Bad Constructor: entries in 'matches[*][0]' and matches[*][1] must not have the same filepath.");
+                throw new Error("Bad Constructor: entries in 'matches[*][0]' and matches[*][1] must not have the same filename.");
             }
             if(match[0].getSubmissionID() === match[1].getSubmissionID()) {
                 throw new Error("Bad Constructor: entries in 'matches[*][0]' and matches[*][1] must not have the same submissionId.");
@@ -56,7 +56,7 @@ export class AnalysisResult implements IAnalysisResult {
         });
 
         this.fileNames = new Map<string, string>();
-        this.fileNames.set(submissionIdA, filePathA).set(submissionIdB, filePathB);
+        this.fileNames.set(submissionIdA, filenameA).set(submissionIdB, filenameB);
     };
     
     getFiles(): Map<string, string> {
