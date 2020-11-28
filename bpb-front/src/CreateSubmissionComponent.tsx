56662d0f79483c9f68cd5b6f4d3885b7c5a64e7d
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-11-28 15:47:07 -0500

    BPB-21 feat: complete correctly rendering create submission component with working file attachment, still need to verify working upload to backend

--- bpb-front/src/CreateSubmissionComponent.tsx
+++ bpb-front/src/CreateSubmissionComponent.tsx
@@ -3,20 +3,33 @@ import { Link } from 'react-router-dom';
 import { store } from './store';
 import createSubmission from './actions/SubmissionAction';
 import Assignment from './types/Assignment';
+import { Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
 //import { createSubmission } from './actions/SubmissionAction';
 
 interface PropsType {
   name: string
-  assignment : Assignment
   files : JSON[]
 }
+
+const propsUpload = {
+  name: 'file',
+  multiple: true,
+  onChange(info : any) {
+    const { status } = info.file;
+    // if (status === 'done') {
+    //   message.success(`${info.file.name} file uploaded successfully.`);
+    // } else if (status === 'error') {
+    //   message.error(`${info.file.name} file upload failed.`);
+    // }
+  },
+};
 
 class CreateSubmissionComponent extends React.Component <PropsType, {}> {
     constructor(props : PropsType) {
         super(props);
         this.state = {
           name: this.props.name,
-          assignment : this.props.assignment,
           files : this.props.files
         };
         this.onInputchange = this.onInputchange.bind(this);
@@ -29,7 +42,7 @@ class CreateSubmissionComponent extends React.Component <PropsType, {}> {
   }
 
   callDispatch() {
-    store.dispatch(createSubmission('ADD_SUBMISSION', this.props.name, this.props.assignment, this.props.files));
+    store.dispatch(createSubmission('UPLOAD_SUBMISSION', this.props.name, store.getState().AssignmentReducer.currentAssignment, this.props.files))
   }
 
   render() {
@@ -40,11 +53,20 @@ class CreateSubmissionComponent extends React.Component <PropsType, {}> {
                   x 
                 </Link>
             </div>
-            <h3>Upload Submission to Assignment {store.getState().AssignmentAction.assignment}</h3>
+            <h3>Upload Submission to Assignment {store.getState().AssignmentReducer.currentAssignment.name}</h3>
             <br/>
             <span text-align="center">
                 <h5>Submission Name:</h5>
                 <input name="name" className='assignment-name-input' type="text" value={this.props.name} onChange={this.onInputchange}/>
+                <Upload {...propsUpload}>
+                    <p className="ant-upload-drag-icon">
+                      <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                    <p className="ant-upload-hint">
+                      Support for single file upload or bulk upload. 
+                    </p>
+                </Upload>      
                 <br/>
                 <Link className='create-assignment-btn btn btn-outline-success mt-2'
                     to="/"
