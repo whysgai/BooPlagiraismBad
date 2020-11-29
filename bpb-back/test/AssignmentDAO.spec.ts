8bc06fc0d4abe5ba6cb0041e582a92a9d8f43b93
Chris Martin <chris_martin@hphc.org>
2020-11-29 13:31:12 -0500

    BPB-31 test: add placeholder test cases

--- bpb-back/test/AssignmentDAO.spec.ts
+++ bpb-back/test/AssignmentDAO.spec.ts
@@ -43,20 +43,26 @@ describe("AssignmentDAO.ts",() => {
     describe("createAssignment()",() => {
         it("Should create an assignment database object if inputs are valid");
         it("Should throw an appropriate error if inputs are invalid");
+        it("Should throw an appropriate error if assignment can't be saved");
     });
 
     describe("readAssignment()",() => {
         it("Should read an assignment database object if {id} is valid");
         it("Should throw an appropriate error when trying to update a nonexistent database object");
+        it("Should throw an appropriate error if assignment can't be found");
     });   
         
     describe("updateAssignment()",() => {
         it("Should update an assignment database object if {id} is valid");
-        it("Should throw an appropriate error if {id} is invalid");
+        it("Should throw an appropriate error if no assignment exists with the specified {id}");
+        it("Should throw an appropriate error if database findOne fails");
+        it("Should throw an appropriate error if database findOneAndUpdate fails");
     });
 
     describe("deleteAssignment()",() => {
         it("Should be able to delete an assignment database object");
         it("Should throw an appropriate error if {id} is invalid");
+        it("Should throw an appropriate error if database findOne fails");
+        it("Should throw an appropriate error if database findOneAndDelete fails");
     });
 });
