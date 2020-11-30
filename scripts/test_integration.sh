6d05a5f1896b94e5fad4d2017c4bd75c3f579f1e
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-30 15:44:42 -0500

    BPB-115 test: shift env vars to test script files

--- scripts/test_integration.sh
+++ scripts/test_integration.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-vagrant ssh -c 'rm -f /vagrant/bpb-back/uploads/README.md'
-echo "Executing integration test for POST /upload ....."
+vagrant rsync
+echo "Executing back-end integration tests....."
 echo ""
-curl -X POST -F 'submissionFile=@README.md' http://192.168.33.10:8080/submissions/sub1/files
-echo ""
-echo "File uploaded (hopefully!): Echoing contents:"
-echo ""
-vagrant ssh -c 'cat /vagrant/bpb-back/uploads/README.md'
+vagrant ssh -c '''
+    export APIPORT=8081;
+    export UPLOADDIRECTORY=/home/vagrant/;
+    export DBCONNECTIONSTRING=mongodb://127.0.0.1:27017/bpbtest;
+    export MAXFILEUPLOADSIZE=1000;
+    export COMPARISONTHRESHOLD=50;
+    npm run integration-test --prefix /vagrant/bpb-back/
+'''
