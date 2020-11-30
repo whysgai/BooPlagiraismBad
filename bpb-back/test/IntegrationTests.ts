2c21b752b2295f0462c2f703b9fd1839166b3b50
Chris Martin <chris_martin@harvardpilgrim.org>
2020-11-30 16:38:33 -0500

    BPB-115 test: fix failing tests

--- bpb-back/test/IntegrationTests.ts
+++ bpb-back/test/IntegrationTests.ts
@@ -2,7 +2,10 @@ import chai, { expect } from 'chai';
 import App from "../src/App";
 import { AppConfig } from "../src/AppConfig";
 import chaiHttp from 'chai-http';
-import { ExpressionContext } from 'java-ast';
+import fs from 'fs';
+import util from 'util';
+const readFileContent = util.promisify(fs.readFile);
+
 /**
  * App Integration Tests
  */
@@ -15,7 +18,7 @@ describe("App (Integration)",() =>  {
         
         baseURI = "http://127.0.0.1:" + AppConfig.port();
         chai.use(chaiHttp);
-        
+
         app = new App(AppConfig.dbConnectionString(),AppConfig.port());
         app.run().then(() => {
             done();
@@ -26,14 +29,50 @@ describe("App (Integration)",() =>  {
         done();
     });
 
-    it('should correctly create a submission (end-to-end)',() => {
+    it('should correctly create a submission (end-to-end)',async () => {
+
+        let fileContentA = await readFileContent("test/res/javaExample.java");
+        let fileContentB = await readFileContent("test/res/javaExample2.java");
+        let fileContentC = await readFileContent("test/res/javaExample3.java");
+        let fileContentD = await readFileContent("test/res/javaExample4.java");
+
         return chai.request(baseURI).post("/Assignments").send({"name":"Test Assignment","submissionIds":[]}).then((createdAssignmentRes) => {
             expect(createdAssignmentRes.status).to.equal(200);
             let assignmentId = createdAssignmentRes.body._id;
 
-            return chai.request(baseURI).post("/Submissions").send({"name":"Test Submission","assignment_id":assignmentId}).then((createdSubmissionRes) => {
-                expect(createdSubmissionRes.status).to.equal(200);
-                let submissionId = createdSubmissionRes.body._id;
+            return chai.request(baseURI).post("/Submissions").send({"name":"Test Submission A","assignment_id":assignmentId}).then((createdSubmissionARes) => {
+                expect(createdSubmissionARes.status).to.equal(200);
+                let submissionIdA = createdSubmissionARes.body._id;
+
+                return chai.request(baseURI).post("/Submissions").send({"name":"Test Submission B","assignment_id":assignmentId}).then((createdSubmissionBRes) => {
+                    expect(createdSubmissionBRes.status).to.equal(200);
+                    let submissionIdB = createdSubmissionBRes.body._id;
+
+                    return chai.request(baseURI).post("/Submissions/" + submissionIdA + "/files")
+                    .attach("submissionFile",fileContentA,"javaExampleA.java").then((fileContentARes) => {
+                        expect(fileContentARes.status).to.equal(200);
+                        
+                        return chai.request(baseURI).post("/Submissions/" + submissionIdB + "/files")
+                        .attach("submissionFile",fileContentB,"javaExampleB.java").then((fileContentBRes) => {
+                            expect(fileContentBRes.status).to.equal(200);
+                            
+                            return chai.request(baseURI).post("/Submissions/" + submissionIdA + "/files")
+                            .attach("submissionFile",fileContentC,"javaExampleC.java").then((fileContentCRes) => {
+                                expect(fileContentCRes.status).to.equal(200);
+
+                                return chai.request(baseURI).post("/Submissions/" + submissionIdB + "/files")
+                                .attach("submissionFile",fileContentD,"javaExampleD.java").then((fileContentDRes) => {
+                                    expect(fileContentDRes.status).to.equal(200);
+                                    
+                                    return chai.request(baseURI).get("/Submissions/compare/" + submissionIdA + "/" + submissionIdB).then((comparisonRes) => {
+                                        expect(comparisonRes.status).to.equal(200);
+                                        console.log(comparisonRes.body);
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
             });
         });
     });
