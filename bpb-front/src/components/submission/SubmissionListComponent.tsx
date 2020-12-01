d18cb103a95a4a8590a1ec89c006684d26df7154
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-12-01 15:48:43 -0500

    BPB-120 feat: pull submissions for an assignment from the backend WORKS

--- bpb-front/src/components/submission/SubmissionListComponent.tsx
+++ bpb-front/src/components/submission/SubmissionListComponent.tsx
@@ -5,25 +5,39 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {store} from '../../store'
 import { compareSubmissions } from '../../actions/ComparisonAction';
+import { readSubmissions } from '../../actions/SubmissionAction'
 
 interface PropTypes {
-  submissions: Submission[]
-  compareEnabled: number
+  //submissions: Submission[]
+  //compareEnabled: number
 }
 
-class SubmissionListComponent extends React.Component <PropTypes, {}> {
+class SubmissionListComponent extends React.Component <PropTypes, {submissions: Submission[], compareEnabled: number}> {
 
   constructor(props : PropTypes) {
     super(props);
     this.state = {
-      submissions : [],
+      submissions : store.getState().SubmissionReducer.submissions,
       compareEnabled: 0
     };
   }
 
+  componentDidMount() {
+    console.log('mount')
+    readSubmissions(store.getState().AssignmentReducer.currentAssignment)
+      .then((submissionAction) => store.dispatch(submissionAction))
+      .then(() => {
+        console.log('inside then did mount')
+        this.setState({
+          submissions : store.getState().SubmissionReducer.submissions
+        })
+      })
+      console.log(this.state.submissions)
+  }
+
   setDisabled() {
     if (store.getState().ComparisonReducer.compareSubmissions.length <= 2) {
-      let newNum = this.props.compareEnabled + 1
+      let newNum = this.state.compareEnabled + 1
       this.setState({
         compareEnabled: newNum
       })
@@ -39,11 +53,19 @@ class SubmissionListComponent extends React.Component <PropTypes, {}> {
       <div className='submission-list' onClick={() => this.setDisabled()}>
         <h3>{store.getState().AssignmentReducer.currentAssignment.name}</h3>
         <Link className='btn btn-outline-success' to='/CreateSubmission'>Upload Submission</Link>
-        <ul>
-          {this.props.submissions.map((submission, index) => 
-            <li key={index}><SubmissionListItemComponent checkboxOn={false} submission={submission} createSubmission={(arg: String) => null}/></li>
-          )}
-        </ul>
+        {
+          this.state.submissions.length > 0 &&
+            <ul>
+              {this.state.submissions.map((submission, index) => 
+                <li key={index}><SubmissionListItemComponent checkboxOn={false} submission={submission} createSubmission={(arg: String) => null}/></li>
+              )}
+            </ul>
+        }
+        {
+          this.state.submissions.length <= 0 &&
+            <h5>No Submissions Exist for this Assignment</h5>
+        }
+        
 
         {
           store.getState().ComparisonReducer.compareSubmissions.length === 0 &&
