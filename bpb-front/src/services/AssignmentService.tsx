fe69fea602775aaccdab43d49b6e4813d789f707
Will Cohen <will.r.cohen@gmail.com>
2020-11-30 20:49:54 -0500

    BPB-119 feat: Add basic fetch request (returns promise)

--- bpb-front/src/services/AssignmentService.tsx
+++ bpb-front/src/services/AssignmentService.tsx
@@ -1,3 +1,5 @@
+import Assignment from "../types/Assignment"
+
 const assignmentURL = 'http://192.168.33.10:8080/'
 
 export function postAssignment(name : string) : void {
@@ -8,4 +10,27 @@ export function postAssignment(name : string) : void {
     })
 }
 
-export default {postAssignment}
+export async function getAssignments() : Promise<Assignment[]> {
+    let response = await fetch(`${assignmentURL}Assignments`);
+    let asJson = await response.json();
+    return Promise.resolve(asJson.assignments as Assignment[]);
+}
+
+// export async function getAssignments() : Promise<any> {
+//     fetch(`${assignmentURL}Assignments`).then((response) => response.json()).then((result) => resolve(result.assignments));
+// }
+
+// export function getAssignments() : Promise<JSON> {
+//     return fetch(`${assignmentURL}Assignments`).then(response => response.json())
+// }
+
+// export function getAssignments() : Promise<any> {
+//     return new Promise((resolve, reject) => {
+//         fetch(`${assignmentURL}Assignments`)
+//             .then((response) => response.json())
+//                 .then((result) => resolve(result.assignments))
+//                     .catch((err) => reject(err))
+//     });    
+// }
+
+export default {postAssignment, getAssignments}
