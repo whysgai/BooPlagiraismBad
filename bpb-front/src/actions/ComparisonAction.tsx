158ecf3d8ffec3216a1258c4565e60a76cccbd1c
Will Cohen <will.r.cohen@gmail.com>
2020-12-03 17:47:41 -0500

    BPB-18 feat: Implement read single submission form server

--- bpb-front/src/actions/ComparisonAction.tsx
+++ bpb-front/src/actions/ComparisonAction.tsx
@@ -1,5 +1,6 @@
 import Submission from '../types/Submission'
 import { getComparison, getFileContent } from '../services/ComparisonService'
+import { getSubmission } from '../services/SubmissionService'
 
 export async function compareSubmissions(compareSubmissions : Submission[]) {
     return {
@@ -23,15 +24,25 @@ export function removeSubmissionComparison(submission : Submission) {
     }    
 }
 
+export async function readComparisonSubmission(submissionId: String) {
+    let submission = await getSubmission(submissionId);
+    return {
+        type: 'ADD_COMPARE',
+        addSubmission: submission
+    }
+}
+
 export async function readFileContent(submission: Submission, type: String) {
     // for each file in submission.files
     // request the file contents and add that to an array of strings
     // then return the object with the type and the array of strings
+    console.log("Action getting files for submission", submission.name, "file names are", submission.files)
+
     let fileContents : String[] = [];
     
     for (let i: number = 0; i < submission.files.length; i++) {
         fileContents.push(await getFileContent(submission._id, i));
-        //console.log("Action: newest file content", fileContents[i]);
+        console.log("Action: newest file content", fileContents[i]);
     }
     return {
         type: type,
