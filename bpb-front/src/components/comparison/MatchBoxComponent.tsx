62d2545658b58bd9bcf12cc60e5cd9507a7ef491
Will Cohen <will.r.cohen@gmail.com>
2020-12-03 23:22:03 -0500

    BPB-18 feat: Render more useful information for each match

--- bpb-front/src/components/comparison/MatchBoxComponent.tsx
+++ bpb-front/src/components/comparison/MatchBoxComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Comparison from '../../types/Comparison';
+import Snippet from '../../types/Snippet';
 
 interface PropsType {
   comparison: Comparison
@@ -16,6 +17,11 @@ class MatchBoxComponent extends React.Component <PropsType, {}> {
     this.state = { };
   }
 
+  clickMatch(match: Snippet[]) {
+    // Use this to make things happen when you click matches
+    console.log("This is a match between files" + match[0].fileName + " and " + match[1].fileName + " on " + match[0].text + " and " + match[1].text)
+  }
+
   render() {
     return (
       <div className="submission-compare-pane border">
@@ -28,7 +34,13 @@ class MatchBoxComponent extends React.Component <PropsType, {}> {
             <ul className="nav flex-column">
               {
                 this.props.comparison.matches.map((match, index) => 
-                  <li className="nav-item" key={index}><span className="nav-item-link">{index+1}</span></li>
+                  <li className="nav-item" key={index} onClick={() => this.clickMatch(match)}>
+                    <span className="nav-item-link">Match {index+1}</span>
+                    <span>Context {match[0].contextType}</span>
+                    <span>{match[0].fileName} lines {match[0].lineNumberStart} - {match[0].lineNumberEnd}</span>
+                    <span>{match[1].fileName} lines {match[1].lineNumberStart} - {match[1].lineNumberEnd}</span>
+
+                  </li>
                 )
               }
             </ul>
