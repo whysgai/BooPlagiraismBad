f97a0c8b4615710eeb5a0759486468d221b2002b
Will Cohen <will.r.cohen@gmail.com>
2020-12-05 11:35:54 -0500

    BPB-140 feat: Set disabled on all other submission list items when two are selected

--- bpb-front/src/components/submission/SubmissionListItemComponent.tsx
+++ bpb-front/src/components/submission/SubmissionListItemComponent.tsx
@@ -15,16 +15,21 @@ class SubmissionListItemComponent extends React.Component <PropsType, {}> {
   
   constructor(props : PropsType) {
     super(props);
-    //TODO: remove this.state since it is not used here
     this.state = {
       submission : this.props.submission,
-      checkboxOn: this.props.checkboxOn,
+      checkboxOn: this.props.checkboxOn
     };
   }
 
+  submissionAlreadySelected() {
+    return store.getState().ComparisonReducer.compareSubmissions.filter(
+      (submission : Submission) =>
+        submission === this.props.submission
+    ).length === 1
+  }
+
   checkbox() {
-    if(store.getState().ComparisonReducer.compareSubmissions.filter((submission : Submission) => 
-    submission === this.props.submission).length === 1){
+    if(this.submissionAlreadySelected()){
       store.dispatch(removeSubmissionComparison(this.props.submission))
       this.setState({
         checkboxOn: false
@@ -41,7 +46,8 @@ class SubmissionListItemComponent extends React.Component <PropsType, {}> {
     return (
       <div className='submission-list-item'>
         <div>
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" 
+          <input type="checkbox" className="form-check-input" id="exampleCheck1"
+          disabled={(!this.submissionAlreadySelected() && store.getState().ComparisonReducer.compareSubmissions.length >= 2)} 
           onClick={() => this.checkbox()} />
         </div>
         <span>
