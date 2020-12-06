952d351a00fe176128f0d8004dab2f36a7f548f9
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-12-06 15:45:09 -0500

    BPB-148 feat: add delete assignment service, this needs to be updated to delete submissions of an assignment as well

--- bpb-front/src/services/AssignmentService.tsx
+++ bpb-front/src/services/AssignmentService.tsx
@@ -1,23 +1,33 @@
 import Assignment from "../types/Assignment"
 
-const assignmentURL = 'http://192.168.33.10:8080/'
+const vagrantURL = 'http://192.168.33.10:8080/'
 
 export function postAssignment(name : string) : void {
-    fetch(`${assignmentURL}Assignments`, {
+    fetch(`${vagrantURL}Assignments`, {
         method: 'POST',
         body: JSON.stringify({'name': name, 'submissionIds': []}),
         headers: {'content-type': 'application/json'}
     })
 }
 
+export function deleteAssignment(assignment: Assignment) : void {
+    fetch(`${vagrantURL}Assignments/${assignment._id}`, {
+        method: 'DELETE',
+        headers: {'content-type': 'application/json'}
+    }).then(response =>
+        response.json().then(json => {
+          return json;
+        }))
+}
+
 export async function getAssignments() : Promise<Assignment[]> {
-    let response = await fetch(`${assignmentURL}Assignments`);
+    let response = await fetch(`${vagrantURL}Assignments`);
     let asJson = await response.json();
     return Promise.resolve(asJson.assignments as Assignment[]);
 }
 
 export async function getAssignment(assignmentId : String) : Promise<Assignment> {
-    let response = await fetch(`${assignmentURL}Assignments/${assignmentId}`);
+    let response = await fetch(`${vagrantURL}Assignments/${assignmentId}`);
     let asJson = await response.json();
     //console.log("get assignment service", asJson)
     return Promise.resolve(asJson as Assignment);
