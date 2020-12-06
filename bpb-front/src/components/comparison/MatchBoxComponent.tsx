36224ad3afa4d522419d25010e47d01e8f15a6f8
Will Cohen <will.r.cohen@gmail.com>
2020-12-06 18:32:16 -0500

    BPB-163 style: Add list styling to matchbox

--- bpb-front/src/components/comparison/MatchBoxComponent.tsx
+++ bpb-front/src/components/comparison/MatchBoxComponent.tsx
@@ -38,23 +38,28 @@ class MatchBoxComponent extends React.Component <PropsType, {}> {
         }        
         {
           (this.props.comparison.matches && this.props.comparison.matches.length > 0 && this.props.comparison.similarityScore < 1) &&
-            <div>
-              <div className="col-12 text-align-center">Similarity: {(this.props.comparison.similarityScore * 100).toFixed(2)}%</div>
-              <div className="col-12 text-align-center">Prominent Matches:</div>
-              <ul className="nav flex-column col-12">
-                {
-                  this.props.comparison.matches.slice(0, 10).map((match, index) => 
-                    <li className="nav-item" key={index} onClick={() => this.selectMatch(match)}>
-                      <ul>
-                        <li className="nav-item-link">Match {index+1}</li>
-                        <li>Context {match[0].contextType}</li>
-                        <li>{match[0].fileName} lines {match[0].lineNumberStart} - {match[0].lineNumberEnd}</li>
-                        <li>{match[1].fileName} lines {match[1].lineNumberStart} - {match[1].lineNumberEnd}</li>
-                      </ul>
-                    </li>
-                  )
-                }
-              </ul> 
+            <div className="card">
+              {/* <div className="card-body"> */}
+              
+                <div className="list-group list-group-flush w-100">
+                  <div className="list-group-item w-100">
+                    <h5 className="card-title text-center">Similarity: {(this.props.comparison.similarityScore * 100).toFixed(2)}%</h5>
+                    <h6 className="card-subtitle text-center">Prominent Matches:</h6>
+                  </div>
+                  {
+                    this.props.comparison.matches.slice(0, 10).map((match, index) => 
+                      <div className="list-group-item w-100" key={index} onClick={() => this.selectMatch(match)}>
+                        {/* <ul> */}
+                          {/* <span className="">Match {index+1}</span> */}
+                          <div>Context {match[0].contextType}</div>
+                          <div>{match[0].fileName} lines {match[0].lineNumberStart} - {match[0].lineNumberEnd}</div>
+                          <div>{match[1].fileName} lines {match[1].lineNumberStart} - {match[1].lineNumberEnd}</div>
+                        {/* </ul> */}
+                      </div>
+                    )
+                  }
+                </div> 
+              {/* </div> */}
             </div>            
         }
         {
