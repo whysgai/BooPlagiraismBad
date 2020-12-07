276cc8ba932e50a69273596335ae6712b9c538d2
Will Cohen <will.r.cohen@gmail.com>
2020-12-06 23:52:19 -0500

    BPB-176 feat: after submitting, cannot edit submission info

--- bpb-front/src/components/submission/CreateSubmissionComponent.tsx
+++ bpb-front/src/components/submission/CreateSubmissionComponent.tsx
@@ -80,29 +80,35 @@ class CreateSubmissionComponent extends React.Component <PropsType, {name: strin
         const { status } = info.file;
       },
       beforeUpload : (file: any) : boolean => {
-        const reader = new FileReader();
-        // Prepares what happens when the reader runs
-        reader.onload = (event) => {
-          if (!event.target) {
-            return false;
-          }
-          file.text = event.target.result;
-          let uploadFiles = this.state.files.concat(file);
-          this.setState({
-            files: uploadFiles
-          });
-          console.log("! Files to upload", this.state.files);
-        };
-        // Actually runs the reader, invoking the above
-        reader.readAsText(file);        
-        return false;
+        if (!this.state.uploaded) {
+          const reader = new FileReader();
+          // Prepares what happens when the reader runs
+          reader.onload = (event) => {
+            if (!event.target) {
+              return false;
+            }
+            file.text = event.target.result;
+            let uploadFiles = this.state.files.concat(file);
+            this.setState({
+              files: uploadFiles
+            });
+            console.log("! Files to upload", this.state.files);
+          };
+          // Actually runs the reader, invoking the above
+          reader.readAsText(file);        
+          return false;
+        } else {
+          return false
+        }
       },
       onRemove : (file: any) => {
-        this.setState(state =>{
-          return {
-            files: this.state.files.filter((f : any) => f.uid !== file.uid)
-          }
-        })
+        if (!this.state.uploaded) {
+          this.setState(state =>{
+            return {
+              files: this.state.files.filter((f : any) => f.uid !== file.uid)
+            }
+          })
+        }
       }
     };
 
@@ -117,14 +123,34 @@ class CreateSubmissionComponent extends React.Component <PropsType, {name: strin
             <br/>
             <span text-align="center">
                 <h5>Submission Name:</h5>
-                <input id="submission-name-input" name="name" className='submission-name-input' type="text" value={this.state.name} onChange={this.onInputchange}/>
-                <Upload {...propsUpload} className='submission-file-input'>
-                    <p className="ant-upload-drag-icon">
-                      <InboxOutlined />
-                    </p>
-                    <p className="ant-upload-text">Click or drag submission files to this area to upload</p>
-                    <p className="ant-upload-hint">Supported file extensions are: .java</p>
-                </Upload>      
+                <input
+                  id="submission-name-input"
+                  name="name"
+                  className='submission-name-input'
+                  type="text" value={this.state.name}
+                  disabled={this.state.uploaded}
+                  onChange={this.onInputchange}/>
+                {
+                  !this.state.uploaded &&
+                    <Upload {...propsUpload} className='submission-file-input'>
+                      <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                      </p>
+                      <p className="ant-upload-text">Click or drag submission files to this area to upload</p>
+                      <p className="ant-upload-hint">Supported file extensions are: .java</p>
+                    </Upload> 
+                }
+                {
+                  this.state.uploaded &&
+                    <ul className="list-group">
+                      {
+                        this.state.files.map((file, index) => 
+                          <li className="list-group-item">{file.name}</li>
+                        )
+                      }
+                    </ul>
+                }  
+                     
                 <br/>
                   {
                     (!this.submissionInfoIsEntered() && !this.state.uploaded) &&
