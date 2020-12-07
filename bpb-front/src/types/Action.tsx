7abf2e535741decf1591058fabdf1fa8f877378a
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-12-06 20:10:10 -0500

    BPB-170 comments: add comments for all types in the client side

--- bpb-front/src/types/Action.tsx
+++ bpb-front/src/types/Action.tsx
@@ -2,6 +2,10 @@ import Submission from "./Submission";
 import Assignment from "./Assignment";
 import Snippet from "./Snippet";
  
+/**
+ * This is an action type. Actions are passed into reducers so any time an action is called this is the 
+ * type associated with it. An action consists of the below properties.
+ */
 type Action = {
  type: String;
  addSubmission: Submission;
--- bpb-front/src/types/Comparison.tsx
+++ bpb-front/src/types/Comparison.tsx
@@ -1,5 +1,10 @@
 import Snippet from "./Snippet"
 
+/**
+ * This is a comparison type. Comparisons are set and updated by a user in the client side based on 
+ * which submissions are being compared.
+ * A comparison consists of a 2D array of files, a 2D array of matches and a similarity score.
+ */
 type Comparison = {
     files: String[][],
     matches: Snippet[][],
