63ecb1696dab09275f2fb07f0f280e145df4040c
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-12-06 19:52:45 -0500

    BPB-169 comments: add comments for reducers in the client side

--- bpb-front/src/reducers/SubmissionReducer.tsx
+++ bpb-front/src/reducers/SubmissionReducer.tsx
@@ -1,6 +1,10 @@
 import Submission from '../types/Submission';
 import Action from "../types/Action"
 
+/**
+ * Set the initial state of the submission reducer. This can be used to read state and set state from a users actions
+ * in the client side.
+ */
 const initialState = {
     submissions: [] as Submission[],
     compareSubmissions: [] as Submission[],
@@ -12,20 +16,22 @@ const initialState = {
     newSubmission: {} as Submission,
 }
 
+/**
+ * The submission reducer allows a user to set the state when reading, uploading and deleting submissions.
+ * @param state the state of the submission reducer.
+ * @param action the action that is being used.
+ */
 const SubmissionReducer = (state = initialState, action : Action) => {
     switch (action.type) {
-
         case 'READ_SUBMISSIONS':
             return {
                 ...state,
                 submissions: action.submissions
             }  
-            
         case 'UPLOAD_SUBMISSION':
             return {
                 ...state,
             }
-
         case 'DELETE_SUBMISSION':
             let submission : Submission
             let newSubmissions : Submission[] = []
