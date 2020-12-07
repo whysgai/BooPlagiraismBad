9e22003cc04f723c1d5373cc4b29c8a550d38f5c
Will Cohen <will.r.cohen@gmail.com>
2020-12-07 11:42:13 -0500

    BPB-164 style: Style create submission page

--- bpb-front/src/components/submission/CreateSubmissionComponent.tsx
+++ bpb-front/src/components/submission/CreateSubmissionComponent.tsx
@@ -120,50 +120,58 @@ class CreateSubmissionComponent extends React.Component <PropsType, ConstructorT
     };
 
     return (
-        <div style={{ textAlign: 'center', paddingTop: '20px' }}>
-            <div style={{textAlign: 'right', paddingRight: '100px', paddingTop: '30px', fontWeight: 'bolder'}}>
-                <Link className="btn btn-outline-danger mt-2" to={`/Assignments/${this.props.match.params.assignmentId}/Submissions`}>
-                  x 
+        <div className="container card mt-3">
+          <div className="card-body">
+            
+            <div className="row mt-2">
+                <div className="col-1"></div>
+                <div className="col-10 text-center">
+                  <h3>Upload Submission to Assignment {store.getState().AssignmentReducer.currentAssignment.name}</h3>
+                </div>
+                <Link className="col-1 btn btn-outline-dark" to={`/Assignments/${this.props.match.params.assignmentId}/Submissions`}>
+                  <i className="fa fa-arrow-circle-left" aria-hidden="true"/> 
                 </Link>
             </div>
-            <h3>Upload Submission to Assignment {store.getState().AssignmentReducer.currentAssignment.name}</h3>
-            <br/>
-            <span text-align="center">
-                <h5>Submission Name:</h5>
-                <input
-                  id="submission-name-input"
-                  name="name"
-                  className='submission-name-input'
-                  type="text" value={this.state.name}
-                  disabled={this.state.uploaded}
-                  onChange={this.onInputchange}/>
-                {
-                  !this.state.uploaded &&
-                    <Upload {...propsUpload} className='submission-file-input'>
-                      <p className="ant-upload-drag-icon">
-                        <InboxOutlined />
-                      </p>
-                      <p className="ant-upload-text">Click or drag submission files to this area to upload</p>
-                      <p className="ant-upload-hint">Supported file extensions are: .java</p>
-                    </Upload> 
-                }
-                {
-                  this.state.uploaded &&
-                    <ul className="list-group">
-                      {
-                        this.state.files.map((file, index) => 
-                          <li className="list-group-item">{file.name}</li>
-                        )
-                      }
-                    </ul>
-                }  
-                     
-                <br/>
+              <div className="row">            
+                <div className="col-3 mt-2"></div>
+              
+                <div className="text-center col-6 mt-2">
+                  <div className="form-group">
+                    <label htmlFor="submission-name-input"><h5 className="text-center col-12 mt-2">Submission Name:</h5></label>
+                    <input
+                      id="submission-name-input"
+                      name="name"
+                      className='submission-name-input form-control'
+                      placeholder="Submission name"
+                      type="text" value={this.state.name}
+                      disabled={this.state.uploaded}
+                      onChange={this.onInputchange}/>
+                  </div>  
+                  {
+                    !this.state.uploaded &&
+                      <Upload {...propsUpload} className='submission-file-input'>
+                        <p className="ant-upload-drag-icon">
+                          <InboxOutlined />
+                        </p>
+                        <p className="ant-upload-text">Click or drag submission files to this area to upload</p>
+                        <p className="ant-upload-hint">Supported file extensions are: .java</p>
+                      </Upload> 
+                  }
+                  {
+                    this.state.uploaded &&
+                      <ul className="list-group">
+                        {
+                          this.state.files.map((file, index) => 
+                            <li className="list-group-item">{file.name}</li>
+                          )
+                        }
+                      </ul>
+                  } 
                   {
                     (!this.submissionInfoIsEntered() && !this.state.uploaded) &&
                     <div>
                       Please enter a name and files. All files must be of the form .java to continue.<br/>
-                      <Link className='create-submission-btn btn btn-outline-secondary disabled mt-2' 
+                      <Link className='create-submission-btn btn btn-outline-secondary disabled col-12 mt-2 mb-2' 
                         onClick={ (event) => {event.preventDefault(); this.alertClick() }} to='#'>
                           Upload Submission
                       </Link>
@@ -171,7 +179,7 @@ class CreateSubmissionComponent extends React.Component <PropsType, ConstructorT
                   }
                   {
                     (this.submissionInfoIsEntered() && !this.state.uploaded) &&
-                    <Link className='create-submission-btn btn btn-outline-success mt-2'
+                    <Link className='create-submission-btn btn btn-success col-12 mt-2 mb-2'
                         to='#'
                         onClick={(event) => {event.preventDefault(); this.callDispatch()}}>
                         Upload Submission
@@ -179,12 +187,15 @@ class CreateSubmissionComponent extends React.Component <PropsType, ConstructorT
                   }
                   {
                     (this.submissionInfoIsEntered() && this.state.uploaded) &&
-                    <Link className='create-submission-btn btn btn-outline-success mt-2'
+                    <Link className='create-submission-btn btn btn-info col-12 mt-2 mb-2'
                         to={`/Assignments/${this.props.match.params.assignmentId}/Submissions`}>
                         Success! Return to assignment
                     </Link>
                   }
-            </span>
+              </div>
+              <div className="col-3 mt-2"></div>
+            </div>  
+          </div>
         </div>
     );
 }
