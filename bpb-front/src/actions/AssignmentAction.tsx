67c7d32b0d71361593156e8637ee66d335ccee80
mikaylawerzanski <werzanski.m@northeastern.edu>
2020-12-06 19:31:29 -0500

    BPB-168 comments: create comments for the action module in the front end

--- bpb-front/src/actions/AssignmentAction.tsx
+++ bpb-front/src/actions/AssignmentAction.tsx
@@ -1,27 +1,43 @@
 import {postAssignment, getAssignments, getAssignment, deleteAssignment} from '../services/AssignmentService'
 import Assignment from '../types/Assignment'
 import { getSubmissionIds, deleteSubmission } from '../services/SubmissionService';
-import { removeSubmissionComparison } from './ComparisonAction';
 
+/**
+ * The create assignment action uses the front end provided name to call a postAssignment() service which sends
+ * the name of an assignment and registers it in the backend.
+ * @param type the type is passed to the reducer to determine which action has been taken.
+ * @param name the name of the assignment the user is creating.
+ */
 export function createAssignment(type : string, name : string) {
     return {
-        type: 'CREATE_ASSIGNMENT',
+        type: type,
         name: name,
         newAssignment: postAssignment(name)
     }
 };
 
+/**
+ * The set current assignment action sends the assignment a user has selected to view to the reducer so that it 
+ * can be set in store.
+ * @param type the type is passed to the reducer to determine which action has been taken.
+ * @param assignment the assignment the user has selected to view the submissions of.
+ */
 export function setCurrentAssignment(type : string, assignment : Assignment) {
-
     return {
-        type: 'SET_CURRENT_ASSIGNMENT',
+        type: type,
         assignment: assignment
     }
 };
 
+/**
+ * Setting current assignment from ID takes the assignment ID from the users current URL and sets the current
+ * assignment in store based on the assignment ID. This is used when a user refreshes a page so that they do not
+ * lose the assignment they were viewing.
+ * @param type the type is passed to the reducer to determine which action has been taken.
+ * @param assignmentId the assignment ID the user is viewing the submissions of.
+ */
 export function setCurrentAssignmentFromId(type : string, assignmentId : String) {
     return getAssignment(assignmentId).then(async (assignment: Assignment) => {
-        //console.log("action set assignment from id", assignment)
         return {
             type: type,
             assignment: assignment
@@ -29,6 +45,9 @@ export function setCurrentAssignmentFromId(type : string, assignmentId : String)
     });
 };
 
+/**
+ * The read assignments action reads the assignments from the back end by using the getAssignments() assignment service
+ */
 export function readAssignments() {
     return getAssignments().then((assignments) => {
         return {
@@ -38,12 +57,19 @@ export function readAssignments() {
     });
 }
 
+/**
+ * The remove assignment action employs the getSubmissionIds() service to remove all submissions in the server that
+ * are associated with the assignment being removed. It then uses the deleteAssignment() service to delete the
+ * assignment from the server. It returns the reducer type and assignment being deleted so that it can be removed
+ * from the store as well.
+ * @param assignment the assignment that is being removed from the backend and from the assignment list.
+ */
 export function removeAssignment(assignment: Assignment) : any {
     getSubmissionIds(assignment._id).then((submissionIds) => {
         for (let submissionId of submissionIds) {
             deleteSubmission(submissionId)
         }
-    }) //removed return on getSubmissionIds, this may need some refactoring if async is messed up
+    })
     deleteAssignment(assignment)
     return {
         type: 'DELETE_ASSIGNMENT',
