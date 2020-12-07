5550c7a5e50fd35905b7a3f1fe6020f9b8ec235c
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-12-07 01:56:53 -0500

    BPB-179 fix: added MAXMATCHESPERFILE to test_integration.sh

--- scripts/test_integration.sh
+++ scripts/test_integration.sh
@@ -7,5 +7,6 @@ vagrant ssh -c '''
     export DBCONNECTIONSTRING=mongodb://127.0.0.1:27017/bpbtest;
     export MAXFILEUPLOADSIZE=300000;
     export COMPARISONTHRESHOLD=50;
+    export MAXMATCHESPERFILE=10
     npm run integration-test --prefix /vagrant/bpb-back/
 '''
